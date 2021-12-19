642e3891c2fc979f155adecb92fbce1aac281617
Kyle <hijongwon@gmail.com>
2021-12-19 22:39:49 +0900

    before using rombok?

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 //  private final MemberRepository memberRepository = new MemoryMemberRepository();
   private final MemberRepository memberRepository;
 
+  @Autowired // 마치 ac.getBean(MemberRepository.class)을 자동으로 입력해준 것처럼 처럼 매개변수에 주입"받아"
   public MemberServiceImpl(MemberRepository memberRepository) {
     this.memberRepository = memberRepository;
   }
